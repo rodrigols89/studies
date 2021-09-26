d3ebb5b91e4b5be7f77ffa80e413ec1a2f06a77a
drigols <drigols.creative@gmail.com>
2021-09-26 13:24:32 -0300

    :rocket: Add Pie Char intro

--- modules/math-codes/modules/statistics-and-probability/src/pie_chart.py
+++ modules/math-codes/modules/statistics-and-probability/src/pie_chart.py
@@ -1,10 +1,15 @@
+########################################################
+# Rodrigo Leite - drigols                              #
+# Last update: 26/09/2021                              #
+########################################################
+
 from matplotlib import pyplot as plt
 import statsmodels.api as sm
 
 df = sm.datasets.get_rdataset('GaltonFamilies', package='HistData').data
-genderCounts = df['gender'].value_counts() # Conta e pega os valores de gêneros - male/female
+genderCounts = df['gender'].value_counts() # Count and take gender values (Male/Female)
 
 genderCounts.plot(kind='pie', title='Gender Counts', figsize=(6,6))
 plt.legend()
-plt.savefig('../images/plot-04.png', format='png')
+plt.savefig('../images/pie-chart.png', format='png')
 plt.show()
