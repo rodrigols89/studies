d2757dfc0bfa41a0c78479dbe9bb529baa17ff88
drigols <drigols.creative@gmail.com>
2021-12-17 22:06:49 -0300

    :rocket: Add math studies

---
+++ modules/math-codes/modules/statistics-and-probability/src/counter_example.py
@@ -0,0 +1,11 @@
+########################################################
+# Rodrigo Leite - drigols                              #
+# Last update: 17/12/2021                              #
+########################################################
+
+from collections import Counter
+
+simplelist = [7, 8, 9, 2, 10, 9, 9, 9, 9, 4, 5, 6, 1, 5, 6, 7, 8, 6, 1, 10]
+
+c = Counter(simplelist)
+print(c)
