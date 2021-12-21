3be2f5075b216eb2298925cdfadf5f484079858d
drigols <drigols.creative@gmail.com>
2021-12-20 21:13:41 -0300

    :rocket: Add intro to testing

---
+++ modules/codigo-avulso-test-tutorial/codigo_avulso_test_tutorial/figura_geometrica.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class FiguraGeometrica(object):
+
+  # Retorna a area da figura
+  def get_area(self):
+    raise NotImplementedError
+
+  # Retorna o perimetro da figura
+  def get_perimetro(self):
+    raise NotImplementedError
---
+++ modules/codigo-avulso-test-tutorial/setup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+setup(
+  name='codigo-avulso-test-tutorial',
+  packages=['codigo_avulso_test_tutorial', 'test'],
+  test_suite='test',
+)
