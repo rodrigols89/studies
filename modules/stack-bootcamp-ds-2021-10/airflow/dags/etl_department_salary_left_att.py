c084e5e45e6b2b9cb45a67c37a50f48df0ab6d07
drigols <drigols.creative@gmail.com>
2021-12-26 16:09:24 -0300

    :rocket: Finish stack project

--- modules/stack-bootcamp-ds-2021-10/airflow/dags/etl_department_salary_left_att.py
+++ modules/stack-bootcamp-ds-2021-10/airflow/dags/etl_department_salary_left_att.py
@@ -1,103 +1,125 @@
 from datetime import datetime,date, timedelta
-import pandas as pd
 from io import BytesIO
-from airflow import DAG
+
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.models import Variable
-from minio import Minio
+from airflow import DAG
+
 from sqlalchemy.engine import create_engine
 
+from minio import Minio
+import pandas as pd
 
+# Argumentos padrão (default) da nossa DAGs:
+# - Dono da DAG (owner);
+# - Se a DAG tem dependência;
+# - A data de início da DAG (Você pode definir um agendamento para ficar sempre consultando)
 DEFAULT_ARGS = {
-    'owner': 'Airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2021, 1, 13),
+  'owner': 'Airflow',
+  'depends_on_past': False,
+  'start_date': datetime(2021, 1, 13),
 }
 
-dag = DAG('etl_department_salary_left_att', 
-          default_args=DEFAULT_ARGS,
-          schedule_interval="@once"
-        )
+# Instancia nossa DAG e passa como argumento:
+# - Nosso dicionário de argumentos default;
+# - Intervelo de execução "@once" = Vai ser executado só uma vez.
+dag = DAG(
+  'etl_department_salary_left_att', 
+  default_args=DEFAULT_ARGS,
+  schedule_interval="@once"
+)
 
+# [Datalake connection]
+# Pega os dados de conexão do Datalake armazenados nas variáveis do Airflow.
 data_lake_server = Variable.get("data_lake_server")
 data_lake_login = Variable.get("data_lake_login")
 data_lake_password = Variable.get("data_lake_password")
 
+# [Database connection]
+# Pega os dados de conexão do Banco de Dados armazenados nas variáveis do Airflow.
 database_server = Variable.get("database_server")
 database_login = Variable.get("database_login")
 database_password = Variable.get("database_password")
 database_name = Variable.get("database_name")
 
-
+# [Cria uma variável que vai representar conexão com o Banco de Dados]
+# Se você prestar bem atenção vai ver que nossa variável é do tipo String.
+# Com isso nós utilizamos o método format() da classe String para fazer um tipo
+# de concatenação ou adição de palavras na nossa String, representando login, senha
+# e etc do Banco de Dados.
 url_connection = "mysql+pymysql://{}:{}@{}/{}".format(
-                 str(database_login)
-                ,str(database_password)
-                ,str(database_server)
-                ,str(database_name)
-                )
+  str(database_login),
+  str(database_password),
+  str(database_server),
+  str(database_name)
+)
 
+# Cria uma instânciada classe Engine, ou seja, conexão com o Banco de Dados.
 engine = create_engine(url_connection)
 
+# Cria uma instância do MinIO com os dados das variáveis acesso armazenadas no Airflow.
 client = Minio(
-        data_lake_server,
-        access_key=data_lake_login,
-        secret_key=data_lake_password,
-        secure=False
-    )
+  data_lake_server,
+  access_key = data_lake_login,
+  secret_key = data_lake_password,
+  secure=False
+)
 
 def extract():
 
-    #query para consultar os dados.
-    query = """SELECT emp.department as department,sal.salary as salary, emp.left
-            FROM employees emp
-            INNER JOIN salaries sal
-            ON emp.emp_no = sal.emp_id;"""
+  # Query para consultar os dados.
+  query = """SELECT emp.department as department,sal.salary as salary, emp.left
+    FROM employees emp
+    INNER JOIN salaries sal
+    ON emp.emp_no = sal.emp_id;"""
 
-    df_ = pd.read_sql_query(query,engine)
+  # Passa a query e conexão com o Banco de Dados.
+  df_ = pd.read_sql_query(query, engine)
     
-    #persiste os arquivos na área de Staging.
-    df_.to_csv( "/tmp/department_salary_left.csv"
-                ,index=False
-            )
+  # Persiste os arquivos na área de Staging.
+  df_.to_csv(
+     "/tmp/department_salary_left.csv",
+     index=False
+  )
 
 def load():
 
-    #carrega os dados a partir da área de staging.
-    df_ = pd.read_csv("/tmp/department_salary_left.csv")
+  # Carrega os dados a partir da área de staging.
+  df_ = pd.read_csv("/tmp/department_salary_left.csv")
 
-    #converte os dados para o formato parquet.
-    df_.to_parquet(
-        "/tmp/department_salary_left.parquet"
-        ,index=False
-    )
+  # Converte os dados para o formato parquet.
+  # Formato parquet é um formato orientado por coluna.
+  df_.to_parquet(
+    "/tmp/department_salary_left.parquet"
+    ,index=False
+  )
 
-    #carrega os dados para o Data Lake.
-    client.fput_object(
-        "processing",
-        "department_salary_left.parquet",
-        "/tmp/department_salary_left.parquet"
-    )
-
+  # Carrega os dados para o Data Lake.
+  client.fput_object(
+    "processing", # Passa para a landing processing.
+    "department_salary_left.parquet",
+    "/tmp/department_salary_left.parquet"
+  )
 
 extract_task = PythonOperator(
-    task_id='extract_data_from_database',
-    provide_context=True,
-    python_callable=extract,
-    dag=dag
+  task_id = 'extract_data_from_database',
+  provide_context = True,
+  python_callable = extract, # Chama a função extract().
+  dag = dag
 )
 
 load_task = PythonOperator(
-    task_id='load_file_to_data_lake',
-    provide_context=True,
-    python_callable=load,
-    dag=dag
+  task_id = 'load_file_to_data_lake',
+  provide_context = True,
+  python_callable = load,  # Chama a função load().
+  dag = dag
 )
 
 clean_task = BashOperator(
-    task_id="clean_files_on_staging",
-    bash_command="rm -f /tmp/*.csv;rm -f /tmp/*.json;rm -f /tmp/*.parquet;",
-    dag=dag
+  task_id = "clean_files_on_staging",
+  bash_command = "rm -f /tmp/*.csv;rm -f /tmp/*.json;rm -f /tmp/*.parquet;",
+  dag = dag
 )
 
 extract_task >> load_task >> clean_task
