74bbb1bdc349809bc8d54fdc5a519d4e6c53d885
drigols <drigols.creative@gmail.com>
2022-01-26 19:29:10 -0300

    :tada: OOP Initial commit

---
+++ modules/oop/modules/oop/README.md
@@ -0,0 +1,8 @@
+# Object-Oriented Programming
+
+![logo](res/logo.jpg)
+
+## Contents
+
+ - [Introdução à Orientação a Objetos (+Classes)](modules/intro-to-oop.md)
+ - [Métodos](modules/methods.md)
---
+++ modules/oop/modules/oop/modules/src/java/Drive.java
@@ -0,0 +1,15 @@
+/**
+ * Rodrigo Leite - drigols
+ * Last update: 22/12/2021  
+ */
+
+class Drive {
+
+  public static void main(String[] args) {
+
+    Person p; // Reference variable.
+    p = new Person(); // Object instance.
+    p.sayHello();
+
+  }
+}
