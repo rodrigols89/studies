924ed69c4567184a9204a157029fb55c8b364075
drigols <drigols.creative@gmail.com>
2023-05-21 02:31:06 -0300

    Remove modules studies

--- modules/python-codes/modules/databases/sqlalchemy/modules/core/src/create_table.py
+++
@@ -1,8 +0,0 @@
-# Authors: Rodrigo Leite <drigols.creative@gmail.com>
-#
-# License: MIT
-from __future__ import annotations
-
-
-def create_table(engine, medaDataModel):
-    medaDataModel.create_all(engine)
