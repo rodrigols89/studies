93b91eb9b6cedf32437850ff3825be15a9c67c2e
drigols <drigols.creative@gmail.com>
2023-12-24 19:26:49 -0300

    Add Time and Space Complexities to Stack studies

--- modules/algorithms-and-ds/modules/data-structures/stacks/src/python/stacks.py
+++ modules/algorithms-and-ds/modules/data-structures/stacks/src/python/stacks.py
@@ -1,7 +1,9 @@
 ##########################################################################
 # Rodrigo Leite da Silva - drigols                                       #
-# Last update: 13/11/2023                                                #
+# Last update: 24/12/2023                                                #
 ##########################################################################
+
+########## ( Stack using Array class ) ##########
 
 
 class StackUsingArray:
@@ -46,7 +48,7 @@ class StackUsingArray:
             print("The Stack is empty.")
             return None
         print("Stack:", self.arr)  # Prin all Stack elements.
-        for current_index in range(self.top + 1):
+        for current_index in range(self.top, -1, -1):
             print(f"Index: {current_index}, Item: {self.arr[current_index]}")
 
     def reverser_word_phrase(self, string):
@@ -59,6 +61,9 @@ class StackUsingArray:
         return reverse
 
 
+########## ( Stack using Linked List class ) ##########
+
+
 class Node:
     def __init__(self, item):
         self.item = item
@@ -67,28 +72,35 @@ class Node:
 
 class StackUsingLinkedList(Node):
     def __init__(self):
-        self.head = None
+        self.top = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
 
     def isEmpty(self):
-        return self.head == None
+        return self.top == None
 
     def push(self, item):
         if self.isEmpty():
-            self.head = Node(item)
+            self.top = Node(item)
+            self.size += 1
         else:
             new_node = Node(item)
-            new_node.next = self.head
-            self.head = new_node
+            new_node.next = self.top
+            self.top = new_node
+            self.size += 1
 
     def pop(self):
         if self.isEmpty():
             print("The Stack is empty.")
             return None
         else:
-            popped_item = self.head.item
-            old_head = self.head
-            self.head = self.head.next
-            del old_head
+            popped_item = self.top.item
+            old_top = self.top
+            self.top = self.top.next
+            self.size -= 1
+            del old_top
             return popped_item
 
     def peek(self):
@@ -96,21 +108,37 @@ class StackUsingLinkedList(Node):
             print("The Stack is empty.")
             return None
         else:
-            return self.head.item
+            return self.top.item
+
+    def traverse(self):
+        if self.isEmpty():
+            print("The Stack is empty.")
+            return None
+        current = self.top
+        while current:
+            print(current.item, end=" ")
+            current = current.next
+        print("")
 
     def reverser_word_phrase(self, string):
         for letter in string:
             self.push(letter)
         reverse = ""
-        while self.head is not None:
+        while self.top is not None:
             popped_item = self.pop()
             reverse += popped_item
         return reverse
 
 
+########## ( Stack using Python Built-In ) ##########
+
+
 class StackUsingPythonBuiltIn:
     def __init__(self):
         self.stack = []
+
+    def __len__(self):
+        return len(self.stack)
 
     def isEmpty(self):
         return self.stack == []
@@ -130,6 +158,15 @@ class StackUsingPythonBuiltIn:
             return None
         # "self.stack[-1]" get the last element of the list (Stack).
         return self.stack[-1]
+
+    def traverse(self):
+        if self.isEmpty():
+            print("The Stack is empty.")
+            return None
+        print("Stack:", self.stack)
+        for item in range(len(self.stack) - 1, -1, -1):
+            print(f"Index: {item}, Item: {self.stack[item]}")
+        print("")
 
     def reverser_word_phrase(self, string):
         for letter in string:
--- modules/algorithms-and-ds/modules/data-structures/stacks/src/python/test.py
+++ modules/algorithms-and-ds/modules/data-structures/stacks/src/python/test.py
@@ -1,4 +1,23 @@
-##########################################################################
-# Rodrigo Leite da Silva - drigols                                       #
-# Last update: 13/11/2023                                                #
-##########################################################################
+from stacks import *
+
+if __name__ == "__main__":
+
+    stack_one = StackUsingArray(100)
+    stack_two = StackUsingLinkedList()
+    stack_three = StackUsingPythonBuiltIn()
+
+    string = input("Word/Phrase to reverse: ")
+    print("Passed Word/Phrase:", string)
+
+    print("\nReversing Word/Phrase using StackUsingArray() class:")
+    reversed_string = stack_one.reverser_word_phrase(string)
+    print("Reversed Word/Phrase:", reversed_string)
+
+    print("\nReversing Word/Phrase using StackUsingLinkedList() class:")
+    reversed_string = stack_two.reverser_word_phrase(string)
+    print("Reversed Word/Phrase:", reversed_string)
+
+
+    print("\nReversing Word/Phrase using StackUsingPythonBuiltIn() class:")
+    reversed_string = stack_three.reverser_word_phrase(string)
+    print("Reversed Word/Phrase:", reversed_string)
