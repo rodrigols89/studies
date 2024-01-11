c29db86f9326dec43fb64273f04789a803aef7c3
drigols <drigols.creative@gmail.com>
2024-01-10 21:57:03 -0300

    Add compilation notes

--- modules/cpp-codes/modules/compilation/src/external_code.cpp
+++
@@ -1,6 +0,0 @@
-#include "external_code.h"
-
-void externalFunction()
-{
-    std::cout << "This is a function from external_code.h" << std::endl;
-}
--- modules/cpp-codes/modules/compilation/src/external_code.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXTERNAL_CODE_H
-#define EXTERNAL_CODE_H
-
-#include <iostream>
-
-void externalFunction();
-
-#endif // EXTERNAL_CODE_H
