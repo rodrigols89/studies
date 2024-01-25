55c1bfdfb08f781f618d52944343bceb006fc7fc
drigols <drigols.creative@gmail.com>
2024-01-25 00:59:42 -0300

    Add Complexities notes from Graph studies

--- modules/algorithms-and-ds/modules/data-structures/graphs/src/python/graphs.py
+++ modules/algorithms-and-ds/modules/data-structures/graphs/src/python/graphs.py
@@ -1,6 +1,6 @@
 ##########################################################################
 # Rodrigo Leite da Silva - drigols                                       #
-# Last update: 01/12/2023                                                #
+# Last update: 25/01/2024                                                #
 ##########################################################################
 
 
@@ -14,25 +14,25 @@ class AdjacencyMatrix:
             print(row)
 
     def add_edge(self, source, destination):
-        self.adj_matrix[source][destination] = 1
-        self.adj_matrix[destination][source] = 1
+        self.matrix[source][destination] = 1
+        self.matrix[destination][source] = 1
 
     def add_edge_based_weight(self, source, destination, weight):
-        self.adj_matrix[source][destination] = weight
-        self.adj_matrix[destination][source] = weight
+        self.matrix[source][destination] = weight
+        self.matrix[destination][source] = weight
 
     def remove_edge(self, source, destination):
-        if self.adj_matrix[source][destination] == 0:
+        if self.matrix[source][destination] == 0:
             print(f"No Edge from {source} to {destination}")
             return
-        self.adj_matrix[source][destination] = 0
-        self.adj_matrix[destination][source] = 0
+        self.matrix[source][destination] = 0
+        self.matrix[destination][source] = 0
 
 
 class AdjNode:
-    def __init__(self, destination):
-        self.destination = destination
-        self.next = None
+    def __init__(self, vertex, next=None):
+        self.vertex = vertex
+        self.next = next
 
 
 class AdjacencyList(AdjNode):
@@ -40,7 +40,7 @@ class AdjacencyList(AdjNode):
         self.num_vertices = num_vertices
         self.edges_list = [None] * self.num_vertices
 
-    def add_edge(self, source, destination):
+    def add_vertex(self, source, destination):
         new_node = AdjNode(destination)
         new_node.next = self.edges_list[source]
         self.edges_list[source] = new_node
@@ -50,14 +50,10 @@ class AdjacencyList(AdjNode):
         self.edges_list[destination] = new_node
 
     def print_adjacency_list(self):
-        for list_index in range(self.num_vertices):
-            current = self.edges_list[list_index]
-            print(f"Adjacency List for vertex {list_index}: ", end="")
+        for index_vertex in range(self.num_vertices):
+            current = self.edges_list[index_vertex]
+            print(f"Adjacency List for vertex {index_vertex}: ", end="")
             while current:
-                print(f"{current.destination} -> ", end="")
+                print(f"{current.vertex} -> ", end="")
                 current = current.next
             print("None")
-
-    def print_adj_list(self):
-        for key in self.edges_list.keys():
-            print("node", key, ": ", self.edges_list[key])
--- modules/algorithms-and-ds/modules/data-structures/graphs/src/python/test.py
+++ modules/algorithms-and-ds/modules/data-structures/graphs/src/python/test.py
@@ -1,18 +1,4 @@
-from graphs import AdjacencyMatrix
-
-if __name__ == "__main__":
-
-    g1 = AdjacencyMatrix(1)
-    print(g1.matrix)
-
-    g2 = AdjacencyMatrix(2)
-    print(g2.matrix)
-
-    g3 = AdjacencyMatrix(3)
-    print(g3.matrix)
-
-    g4 = AdjacencyMatrix(4)
-    print(g4.matrix)
-
-    g5 = AdjacencyMatrix(5)
-    print(g5.matrix)
+##########################################################################
+# Rodrigo Leite da Silva - drigols                                       #
+# Last update: 25/01/2024                                                #
+##########################################################################
