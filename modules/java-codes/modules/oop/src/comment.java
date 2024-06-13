a41f8417e626d054c2ea026cf0bdfe927f7f7ac7
rodrigols89 <rodrigols89.inbox@gmail.com>
2024-06-12 23:07:00 -0300

    Add OOP studies - References variables. - Class Attributes. - Constructors. - Getters and Setters.

---
+++ modules/java-codes/modules/oop/src/comment.java
@@ -0,0 +1,41 @@
+/* class Account {
+
+    // Attributes.
+    private int number;
+    private String holder;
+    private double balance;
+    private double limit;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+}
+ */
